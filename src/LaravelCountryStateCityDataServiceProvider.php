686c175be8f03ac84fbdde446ecc4c9766f44593
Dipesh Sukhia <dipeshsukhia@users.noreply.github.com>
2021-05-07 10:55:44 +0000

    Apply fixes from StyleCI

--- src/LaravelCountryStateCityDataServiceProvider.php
+++ src/LaravelCountryStateCityDataServiceProvider.php
@@ -6,28 +6,27 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelCountryStateCityDataServiceProvider extends ServiceProvider
 {
-    const STUB_DIR = __DIR__ . '/resources/stubs/';
+    const STUB_DIR = __DIR__.'/resources/stubs/';
 
     /**
      * Bootstrap the application services.
      */
     public function boot()
     {
-
         if ($this->app->runningInConsole()) {
             /* model */
             if (is_dir(base_path('app/Models'))) {
                 $modelDir = base_path('app/Models');
-                $modelNameSpace = "\\Models";
+                $modelNameSpace = '\\Models';
             } else {
                 $modelDir = base_path('app');
-                $modelNameSpace = "";
+                $modelNameSpace = '';
             }
 
             foreach (['Country', 'State', 'City'] as $modelName) {
-                $ModelTemplate = self::getStubContents("Models/".$modelName.".stub");
+                $ModelTemplate = self::getStubContents('Models/'.$modelName.'.stub');
                 $ModelTemplate = str_replace('{{modelNameSpace}}', $modelNameSpace, $ModelTemplate);
-                file_put_contents($modelDir . "/".$modelName.".php", $ModelTemplate);
+                file_put_contents($modelDir.'/'.$modelName.'.php', $ModelTemplate);
             }
 
             /* model */
@@ -35,7 +34,7 @@ class LaravelCountryStateCityDataServiceProvider extends ServiceProvider
             /* seeders */
             if (is_dir(database_path('seeds'))) {
                 $seedDir = database_path('seeds');
-                $seederNameSpace = "";
+                $seederNameSpace = '';
             } else {
                 $seedDir = database_path('seeders');
                 $seederNameSpace = "namespace Database\\Seeders;\n";
@@ -43,14 +42,14 @@ class LaravelCountryStateCityDataServiceProvider extends ServiceProvider
             $seederTemplate = self::getStubContents('CountryStateCityTableSeeder.stub');
             $seederTemplate = str_replace('{{seederNameSpace}}', $seederNameSpace, $seederTemplate);
             $seederTemplate = str_replace('{{modelNameSpace}}', $modelNameSpace, $seederTemplate);
-            file_put_contents($seedDir . '/CountryStateCityTableSeeder.php', $seederTemplate);
+            file_put_contents($seedDir.'/CountryStateCityTableSeeder.php', $seederTemplate);
             /* seeders */
 
             $this->publishes([
-                __DIR__ . '/resources/migrations/2014_02_04_000000_create_country_state_city_table.stub' => database_path('migrations/2014_02_04_000000_create_country_state_city_table.php'),
-                __DIR__ . '/resources/DataProviders/CountryDataProvider.stub' => app_path('DataProviders/CountryDataProvider.php'),
-                __DIR__ . '/resources/DataProviders/StateDataProvider.stub' => app_path('DataProviders/StateDataProvider.php'),
-                __DIR__ . '/resources/DataProviders/CityDataProvider.stub' => app_path('DataProviders/CityDataProvider.php'),
+                __DIR__.'/resources/migrations/2014_02_04_000000_create_country_state_city_table.stub' => database_path('migrations/2014_02_04_000000_create_country_state_city_table.php'),
+                __DIR__.'/resources/DataProviders/CountryDataProvider.stub' => app_path('DataProviders/CountryDataProvider.php'),
+                __DIR__.'/resources/DataProviders/StateDataProvider.stub' => app_path('DataProviders/StateDataProvider.php'),
+                __DIR__.'/resources/DataProviders/CityDataProvider.stub' => app_path('DataProviders/CityDataProvider.php'),
             ], 'LaravelCountryStateCityData');
         }
     }
@@ -60,11 +59,10 @@ class LaravelCountryStateCityDataServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
     }
 
     private function getStubContents($stubName)
     {
-        return file_get_contents(self::STUB_DIR . $stubName);
+        return file_get_contents(self::STUB_DIR.$stubName);
     }
 }
